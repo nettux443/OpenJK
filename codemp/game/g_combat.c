5736eed945d47535509f0a418d206f47855d1713
lbourne <lbourne@pc1.lewis.home.lan>
2017-04-22 12:45:38 +0100

    udefault to jump and saber only. Get PDuel wins even if you die but the team wins

--- codemp/game/g_combat.c
+++ codemp/game/g_combat.c
@@ -2039,8 +2039,9 @@ void G_AddPowerDuelScore(int team, int score)
 		check = &g_entities[i];
 		if (check->inuse && check->client &&
 			check->client->pers.connected == CON_CONNECTED && !check->client->iAmALoser &&
-			check->client->ps.stats[STAT_HEALTH] > 0 &&
-			check->client->sess.sessionTeam != TEAM_SPECTATOR &&
+                        // nettux if the team wins then you get a point, even if you died before the end of the round
+			/**check->client->ps.stats[STAT_HEALTH] > 0 &&
+			check->client->sess.sessionTeam != TEAM_SPECTATOR &&**/
 			check->client->sess.duelTeam == team)
 		{ //found a living client on the specified team
 			check->client->sess.wins += score;
